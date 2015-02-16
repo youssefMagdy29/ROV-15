514c55789930f649e50b13e6d5e49b7b040b675b
youssefMagdy29 <youssef.magdy29@gmail.com>
2015-02-16 14:54:30 +0200

    Initial commit

---
+++ ROV15/mainwindow.cpp
@@ -0,0 +1,98 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QKeyEvent>
+
+QLabel *l;
+//Directions...
+    //Commands...
+    QString const FORWARD         = "f";
+    QString const BACKWARD        = "b";
+    QString const MOVE_RIGHT      = "x";
+    QString const MOVE_LEFT       = "c";
+    QString const TURN_RIGHT      = "r";
+    QString const TURN_LEFT       = "l";
+    QString const UP              = "u";
+    QString const DOWN            = "d";
+    QString const STOP_HORIZONTAL = "h";
+    QString const STOP_VERTICAL   = "v";
+    //Keys...
+    QChar const KEY_FORWARD    = 'W';
+    QChar const KEY_BACKWARD   = 'S';
+    QChar const KEY_MOVE_RIGHT = 'D';
+    QChar const KEY_MOVE_LEFT  = 'A';
+    QChar const KEY_TURN_RIGHT = 'K';
+    QChar const KEY_TURN_LEFT  = 'L';
+    QChar const KEY_UP         = 'I';
+    QChar const KEY_DOWN       = 'J';
+
+//Light..
+    //Commands...
+    QString const LIGHT_ON  = "n";
+    QString const LIGHT_OFF = "m";
+
+    //Keys...
+    QChar const KEY_LIGHT_ON  = 'N';
+    QChar const KEY_LIGHT_OFF = 'M';
+
+//Camera..
+    //Commands...
+    QString const CAM_RIGHT = "y";
+    QString const CAM_LEFT  = "t";
+    QString const CAM_STOP  = "o";
+
+    //Keys...
+    QChar const KEY_CAM_RIGHT = 'Y';
+    QChar const KEY_CAM_LEFT  = 'T';
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    l = ui->labelSentCommand;
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+void MainWindow::keyPressEvent(QKeyEvent *e) {
+    QChar k = e->key();
+    if (k == KEY_FORWARD) {
+        l->setText(FORWARD);
+    }
+    else if (k == KEY_BACKWARD) {
+        l->setText(BACKWARD);
+    }
+    else if (k == KEY_MOVE_RIGHT) {
+        l->setText(MOVE_RIGHT);
+    }
+    else if (k == KEY_MOVE_LEFT) {
+        l->setText(MOVE_LEFT);
+    }
+    else if (k == KEY_TURN_RIGHT) {
+        l->setText(TURN_RIGHT);
+    }
+    else if (k == KEY_TURN_LEFT) {
+        l->setText(TURN_LEFT);
+    }
+    else if (k == KEY_UP) {
+        l->setText(UP);
+    }
+    else if (k == KEY_DOWN) {
+        l->setText(DOWN);
+    }
+}
+
+void MainWindow::on_buttonConnect_clicked()
+{
+    ui->labelConnectionStatus->setText("Connected");
+    ui->labelConnectionStatus->setStyleSheet("color: #00ff00");
+}
+
+void MainWindow::on_buttonDisconnect_clicked()
+{
+    ui->labelConnectionStatus->setText("Disconnected");
+    ui->labelConnectionStatus->setStyleSheet("color: #ff0000");
+}
