0a50a1bf6b315ceaab8ad82c3ea1543ed73cb4ba
youssefMagdy29 <youssef.magdy29@gmail.com>
2015-03-14 14:49:41 +0200

    first version of joystick.h

--- ROV15/joystick.h
+++ ROV15/joystick.h
@@ -6,16 +6,35 @@ class Joystick
     Q_OBJECT
 
 public:
-    explicit Joystick(int joystick_number);
+    explicit Joystick(int joystickNumber);
     ~Joystick();
 
-    static enum JOYSTICK{
+    static enum JOYSTICK {
         JOYSTICK_1 = 0,
         JOYSTICK_2 = 1
     } JOYSTICK;
 
-private:
+    static enum BUTTON {
+        JOYSTICK_1      = 0,
+        JOYSTICK_2      = 1,
+        JOYSTICK_3      = 2,
+        JOYSTICK_4      = 3,
+        JOYSTICK_R1     = 5,
+        JOYSTICK_R2     = 7,
+        JOYSTICK_L1     = 4,
+        JOYSTICK_L2     = 6,
+        JOYSTICK_START  = 9,
+        JOYSTICK_SELECT = 8
+    } BUTTON;
 
+private:
+    bool buttonState[32];
+
+signals:
+    void connected();
+    void disconnected();
+    void buttonPressed();
+    void buttonReleased();
 };
 
 #endif // JOYSTICK_H
